fc89f165a412699202155e82363147e6ba1758fb
Juergen Reiss <juergen.reiss@nokia.com>
2023-06-12 09:05:12 +0200

    Reusable on_attach keymaps

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -10,6 +10,13 @@ return {
     }
     -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local custom_attach = function(client, bufnr)
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'lr', vim.lsp.buf.rename, bufopts)
+    end
     -- The following example advertise capabilities to `clangd`.
     require'lspconfig'.clangd.setup {
       capabilities = capabilities,
@@ -20,15 +27,11 @@ return {
           "--pch-storage=memory",
           "--clang-tidy"
       },
-      on_attach = function(client, bufnr)
-        local bufopts = { noremap=true, silent=true, buffer=bufnr }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', 'lr', vim.lsp.buf.rename, bufopts)
-      end 
+      on_attach = custom_attach,
     }
-    require'lspconfig'.pyright.setup{}
+    require'lspconfig'.pyright.setup{
+         on_attach = custom_attach,
+    }
     vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
   end
 }
